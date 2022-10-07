46446ab3ff8dbfb73606edc8c00b02f9bd1f87b4
DaniGM17 <111198402+DaniGM17@users.noreply.github.com>
2022-10-06 21:04:00 -0500

    Delete 18-LocalStorage directory

--- 18-LocalStorage/js/03-app.js
+++
@@ -1,9 +0,0 @@
-localStorage.removeItem('nombre');
-//Para actualizar un registro
-const mesesArray = JSON.parse(localStorage.getItem('meses'));
-console.log(mesesArray);
-mesesArray.push('Junio');
-console.log(mesesArray);
-localStorage.setItem('meses', JSON.stringify(mesesArray));
-
-localStorage.clear();
